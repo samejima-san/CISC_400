260d49b59a423b5b9e65ec34a37392b3bd3d0ff2
samejima-san <samejimasama@gmail.com>
2022-09-06 16:32:13 -0400

    added the download files - adjusted references

---
+++ exercises/exercise_02/main.js
@@ -0,0 +1,11 @@
+function main(){
+    let canvas = document.getElementById("webgl");
+    let gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
