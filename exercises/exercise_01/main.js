d351ebcb03ebaff8536eb599841120d4ba93927d
samejima-san <samejimasama@gmail.com>
2022-09-06 15:59:19 -0400

    centered the circle in the canvas...irrelevant

--- exercises/exercise_01/main.js
+++ exercises/exercise_01/main.js
@@ -10,6 +10,6 @@ function main(){
     }
 
     ctx.fillStyle = "rgba(0, 0, 255, 1.0)";
-    ctx.arc(100, 75, 50, 0, 2 * Math.PI);
+    ctx.arc(width/2, height/2, 50, 0, 2 * Math.PI);
     ctx.fill();
 }
